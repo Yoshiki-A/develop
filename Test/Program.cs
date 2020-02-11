f2f07046ada2f0866d774efb425acf44f7a366d7
yoshiki ara <dena0225@outlook.jp>
2020-02-11 20:51:32 +0900

    疎通確認

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,6 +17,7 @@ namespace Test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
